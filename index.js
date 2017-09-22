546628587040ea586327c1711b489e5ce2028fd9
T <theresa.hindersinn@gmail.com>
2017-09-22 14:17:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,31 +12,63 @@ function setCart(c) {
 function addToCart(item) {
   var itemPrice = Math.floor((Math.random()*100)+1);
   cart.push({[item]: itemPrice});
-  /* I can't seem to get the price to be recognized as an interger*/
  console.log(`${item} has been added to your cart.`);
  return cart;
 }
 
 function viewCart() {
   var fullCart = [];
-  if (cart.length < 1){
-    console.log("Your shopping cart is empty.");
-    return cart;
-  }else {
-    for(var x = 0; cart.length > x; x++){
-      fullCart.push(cart[x] + " at $" + x.price);
+  if(cart.length === 0){
+    console.log('Your shopping cart is empty.');
+  } else{
+    for(var x = 0; x<cart.length; x++){
+      var object = cart[x];
+      var itemName = Object.keys(object);
+      var itemPrice = object[itemName];
+        if(cart.length === 1){
+          console.log('In your cart, you have ' + itemName + ' at $' + itemPrice + '.');
+        }
+        if ((cart.length - 1 === x)){
+          fullCart.push(`and ${itemName} at $${itemPrice}`);
+        }
+        else {
+          fullCart.push(`${itemName} at $${itemPrice}`);
+        }
     }
-    return "In your cart, you have" + fullCart + "."
+    if (cart.length === 2){
+      console.log(`In your cart, you have ${fullCart.join(' ')}.`)
+}
+  else{
+    console.log(`In your cart, you have ${fullCart.join(', ')}.`)
   }
-  // write your code here
+  }
+
 }
 
 function total() {
-  // write your code here
+  var total = 0
+  for(var a = 0; cart.length>a; a++){
+    var object = cart[a];
+    var itemName = Object.keys(object);
+    var itemPrice = object[itemName];
+    total = total + itemPrice
+  }
+  return total;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for (var b = 0; b<cart.length; b++){
+    var object = cart[b];
+    var itemName = Object.keys(object);
+    var itemPrice = object[itemName];
+    if (itemName === item){
+      cart.splice(b, 1);
+      console.log(cart);
+    }
+    else{
+      console.log('That item is not in your cart.' + viewCart());
+    }
+  }
 }
 
 function placeOrder(cardNumber) {
